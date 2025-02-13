2ee2a74a6d310dd47b80d772a991cfe225534893
TingelTanne <fuchs.tristan@yahoo.de>
2025-02-13 16:50:28 +0100

    wordcounter implemented

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+def wordcounter(whole_text):
+    word_count = 0
+    word_count = len(whole_text.split())
+    print(word_count)
+
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-
-    print(file_contents)
+    wordcounter(file_contents)
+    
 
 main()
